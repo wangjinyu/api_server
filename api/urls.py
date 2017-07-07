15566b1694dfe0ea19715cfe4e253ad8aa11eb2e
wangjinyu <jinyuid@gmail.com>
2017-07-07 16:28:16 +0800

    test

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.current_datetime, name='current_datetime')
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the api index.")
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
