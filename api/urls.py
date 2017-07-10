a7124f2d50dcb9e544110fc704c91452e9fc63df
wangjinyu <jinyuid@gmail.com>
2017-07-10 14:18:14 +0800

    错误修改

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ from api import views
 urlpatterns = [
     url(r'^hello/', views.hello, name='hello'),
     url(r'^$', views.hello, name='hello'),
-    url(r'^saySomthing/', views.saySomthing, name='saySomthing')
-    url(r'^current_time/', views.current_time, name='current_time')
+    url(r'^saySomthing/', views.saySomthing, name='saySomthing'),
+    url(r'^current_time/', views.current_time, name='current_time'),
 ]
