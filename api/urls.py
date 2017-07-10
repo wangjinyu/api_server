5017256575c362f10e38ae11ab1f6b09bbb8db3c
wangjinyu <jinyuid@gmail.com>
2017-07-10 14:21:21 +0800

    删除测试接口

--- api/urls.py
+++ api/urls.py
@@ -6,7 +6,4 @@ from api import views
 urlpatterns = [
     url(r'^hello/', views.hello, name='hello'),
     url(r'^$', views.hello, name='hello'),
-    url(r'^saySomthing/', views.saySomthing, name='saySomthing'),
-    url(r'^current_time/', views.current_time, name='current_time'),
-    url(r'^home/', views.home, name='home'),
 ]
--- api/views.py
+++ api/views.py
@@ -3,13 +3,3 @@ from django.http import *
 # Create your views here.
 def hello(request):
     return HttpResponse("Hello, my first api.")
-
-def saySomthing(request):
-    return HttpResponse("restart test.")
-
-
-def current_time(request):
-    return HttpResponse("将要显示当前时间")
-
-def home(request):
-    return HttpResponse("home")
