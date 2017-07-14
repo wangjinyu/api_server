6d4d0708f18177f6180f24fe93bf10f19dd21f59
wangjinyu <jinyuid@gmail.com>
2017-07-14 13:12:37 +0800

    修改language code

--- api_server/settings.py
+++ api_server/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'zh_hans'
+LANGUAGE_CODE = 'zh-hans'
 
 TIME_ZONE = 'Asia/Shanghai'
 
